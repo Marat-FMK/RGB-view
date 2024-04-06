fdf1994b37422d19fde82b6397c5248d620cb142
Marat-FMK <kosjachok8@yandex.ru>
2024-04-06 05:24:11 +0300

    work with view

--- RGB view/ViewController.swift
+++ RGB view/ViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var viewRGB: UIView!
+    
+    @IBOutlet var rValue: UILabel!
+    
+    @IBOutlet var gValue: UILabel!
+    
+    @IBOutlet var bValue: UILabel!
+    
+    @IBOutlet var rSliderOutlet: UISlider!
+    
+    @IBOutlet var bSliderOutlet: UISlider!
+    
+    @IBOutlet var gSliderOutlet: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        viewRGB.layer.cornerRadius = 10
+        viewRGB.backgroundColor = .black
+        rValue.text = ""
+        gValue.text = ""
+        bValue.text = ""
+        
+        rSliderOutlet.value = 0
+        gSliderOutlet.value = 0
+        bSliderOutlet.value = 0
+       
     }
 
-
+    
+    @IBAction func rSlider() {
+        rValue.text = String(rSliderOutlet.value)
+    }
+    
+    @IBAction func gSlider() {
+        gValue.text = String(gSliderOutlet.value)
+    }
+    
+    
+    @IBAction func bSlider() {
+        bValue.text = String(bSliderOutlet.value)
+    }
+    
 }
 
